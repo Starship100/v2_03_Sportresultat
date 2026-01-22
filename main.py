93b2a1bc6c3fe71b28dd546d498666dd6ab186de
Starship100 <jimmy.widlund@outlook.com>
2026-01-22 21:06:58 +0100

    Fotbollsresultat

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 
 
 print("Match: Tottenham vs Liverpool")
+tottenham_goals = int(input("Hur många mål gjorde Tottenham? "))
+liverpool_goals = int(input("Hur många mål gjorde Liverpool? "))
+
+if tottenham_goals == liverpool_goals:
+    print("Det blev oavgjort!")
+elif tottenham_goals > liverpool_goals:
+    print("Tottenham vann!")
+elif tottenham_goals < liverpool_goals:
+    print("Liverpool vann!")
